227ff4ff527d7901f440134980b90a53bde343f3
axelabbas <Fadeloy45@gmail.com>
2022-09-19 23:08:43 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fulltoters/home.dart';
-import 'package:fulltoters/navbar.dart';
+import 'package:fulltoters/ui/screens/mainscreens/home.dart';
+import 'package:fulltoters/ui/screens/navbar.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: myNavbar(),debugShowCheckedModeBanner: false,
+    home: myNavbar(),
+    debugShowCheckedModeBanner: false,
   ));
 }
